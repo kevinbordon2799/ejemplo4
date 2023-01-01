6a76ae033496030b615cca88ed47bfc2aa3b7b7a
kevinbordon2799 <kevinbordon2016@hotmail.com>
2023-01-01 20:56:37 -0300

    Se actualizó la app

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const path = require("path");
 const fs = require("fs");
 
-const FILE_NAME = "persons.json";
-const FILE_PATH = path.join(__dirname, FILE_NAME);
+const readPersonsFile = function () {
+    const FILE_NAME = "persons.json";
+    const FILE_PATH = path.join(__dirname, FILE_NAME);
 
-fs.readFile(FILE_PATH, { encoding: "utf-8" }, function (err, data) {
-    console.log({ err, data: JSON.parse(data) });
-});
+    fs.readFile(FILE_PATH, { encoding: "utf-8" }, function (err, data) {
+        console.log({ err, data: JSON.parse(data) });
+    });
+};
+
+const readTeamsFile = function () {
+    const FILE_NAME = "teams.json";
+    const FILE_PATH = path.join(__dirname, FILE_NAME);
+
+    fs.readFile(FILE_PATH, { encoding: "utf-8" }, function (err, data) {
+        console.log({ err, data: JSON.parse(data) });
+    });
+};
+
+readPersonsFile();
+readTeamsFile();
